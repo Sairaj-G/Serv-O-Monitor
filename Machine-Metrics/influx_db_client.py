9f96f1dbb5226f05f91943666c1238be9fb95e5e
Kruthikesh Kopparam <kruthikeshm@gmail.com>
2024-12-10 19:15:14 +0530

    get request modified for all the metrics according to time and ip of the client

--- Machine-Metrics/influx_db_client.py
+++ Machine-Metrics/influx_db_client.py
@@ -2,31 +2,36 @@ from data_models import *
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+import socket
 
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
 
-token = "W_yCgnGlWTh-SpqZZ_alZ7ZUYBo3xf02z6AteuqYRthqQlu6xOpQf7PzRwh8dEgQ15DBDDV7MPq-x2CIF4hVYA=="
-org = "ORG_NAME"
+token = "4nv73YDP8FmP27gspMhYpar4LRYRmf0bmtwEYHVYjeUhieXNxYXPeqTvL3LDSws5trwYhSwhwxqX_fdGwdE8bQ=="
+org = "Servo-monitor"
 url = "http://localhost:8086"
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = write_client.write_api(write_options=SYNCHRONOUS)
-bucket = "BUCKET_NAME"
-
+bucket = "metrics_data"
 
 def log_cpu_metric():
     cpu_metric = CPUMetrics()
+
     point = Point("cpu_metrics") \
+    .tag("IPaddress", f"{IPAddr}")\
     .field("util_percent", cpu_metric.util_percent) \
     .field("num_logical_cores", cpu_metric.num_logical_cores) \
     .field("num_physical_cores", cpu_metric.num_physical_cores) \
     .field("cpu_freq", cpu_metric.cpu_freq) \
     .field("max_freq", cpu_metric.max_freq) \
     .field("num_processes", cpu_metric.num_processes)
-        
-    write_api.write(bucket=bucket, org="ORG_NAME", record=point)
+    
+    write_api.write(bucket=bucket, org=org, record=point)
 
 def log_disk_metrics():
     disk_metric = DiskMetrics()
     point = Point("disk_metrics") \
+    .tag("IPaddress", f"{IPAddr}")\
     .field("read_speed", disk_metric.read_speed) \
     .field("write_speed", disk_metric.write_speed)\
     .field("active_time", disk_metric.active_time)
@@ -36,6 +41,7 @@ def log_disk_metrics():
 def log_network_metric():
     network_metric = NetworkMetrics()
     point = Point("network_metrics") \
+    .tag("IPaddress", f"{IPAddr}")\
     .field("host_name", network_metric.host_name) \
     .field("ip_address", network_metric.ip_address) \
     .field("wifi_signal_strength_percent", network_metric.wifi_signal_strength_percent) \
@@ -47,7 +53,8 @@ def log_network_metric():
 
 def log_memory_metrics():
     memory_metric = MemoryMetrics()
-    point = Point("disk_metrics") \
+    point = Point("memory_metrics") \
+    .tag("IPaddress", f"{IPAddr}")\
     .field("ram_memory_total_GB", memory_metric.ram_memory_total_GB) \
     .field("ram_memory_used_GB", memory_metric.ram_memory_used_GB)\
     .field("ram_usage_percent", memory_metric.ram_usage_percent)\
@@ -55,4 +62,3 @@ def log_memory_metrics():
     .field("swap_memory_used_GB", memory_metric.swap_memory_used_GB)
 
     write_api.write(bucket=bucket, org="ORG_NAME", record=point)
-
